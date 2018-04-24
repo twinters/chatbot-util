ccaef17c6b800ce61f39596f9b9a1eef46a47dac
T. Winters <info@thomaswinters.be>
2018-04-24 03:48:22 +0200

    Added function to distinguish between own and other tweets to weighted conversation collector

--- src/main/java/be/thomaswinters/bot/util/WeightedConversationCollector.java
+++ src/main/java/be/thomaswinters/bot/util/WeightedConversationCollector.java
@@ -9,18 +9,35 @@ import java.util.function.BiFunction;
  * Class to help collect conversations of chat messages with weights to every message
  */
 public class WeightedConversationCollector {
-    private final BiFunction<IChatMessage, Integer, Integer> weightMapper;
-    private final int maxMessages;
+    public static final BiFunction<IChatMessage, Integer, Integer> MAP_TO_ONE = (i, j) -> 1;
 
-    public WeightedConversationCollector(int maxMessages, BiFunction<IChatMessage, Integer, Integer> weightMapper) {
+    private final String userName;
+    private final int maxMessages;
+    private final BiFunction<IChatMessage, Integer, Integer> ownMessagesWeightMapper;
+    private final BiFunction<IChatMessage, Integer, Integer> otherMessagesWeightMapper;
+
+    public WeightedConversationCollector(String userName, int maxMessages,
+                                         BiFunction<IChatMessage, Integer, Integer> ownMessagesWeightMapper,
+                                         BiFunction<IChatMessage, Integer, Integer> otherMessagesWeightMapper) {
+        this.userName = userName;
         this.maxMessages = maxMessages;
-        this.weightMapper = weightMapper;
+        this.ownMessagesWeightMapper = ownMessagesWeightMapper;
+        this.otherMessagesWeightMapper = otherMessagesWeightMapper;
+    }
+
+    public WeightedConversationCollector(String username, int maxMessages) {
+        this(username, maxMessages, MAP_TO_ONE, MAP_TO_ONE);
     }
 
     public WordCounter collectConversation(IChatMessage message) {
         WordCounter.Builder b = WordCounter.builder();
         IChatMessage currentMessage = message;
         for (int i = 0; i < maxMessages; i++) {
+
+            BiFunction<IChatMessage, Integer, Integer> weightMapper =
+                    currentMessage.getUser().getScreenName().equals(userName) ?
+                            ownMessagesWeightMapper : otherMessagesWeightMapper;
+
             b.addWeighted(message.getMessage(), weightMapper.apply(message, i));
             if (currentMessage.getPrevious().isPresent()) {
                 currentMessage = currentMessage.getPrevious().get();
