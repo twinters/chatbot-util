9ae25fdf545e2cec1687c38b406b25709a537382
T. Winters <info@thomaswinters.be>
2018-04-24 04:22:34 +0200

    Added Chatbox

---
+++ src/main/java/be/thomaswinters/chatbot/data/ChatBox.java
@@ -0,0 +1,25 @@
+package be.thomaswinters.chatbot.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
+public class ChatBox {
+    private IChatMessage latestMessage = null;
+    private List<BiConsumer<ChatBox, IChatMessage>> newChatConsumers = new ArrayList<>();
+
+    public void postReaction(IChatUser user, String message) {
+        IChatMessage chatMessage = new ChatMessage(Optional.ofNullable(latestMessage), message, user);
+        this.latestMessage = chatMessage;
+        newChatConsumers.forEach(e -> e.accept(this, chatMessage));
+    }
+
+    public void addNewChatMessageListener(BiConsumer<ChatBox, IChatMessage> listener) {
+        if (listener == null) {
+            throw new IllegalArgumentException("Listener can not be null");
+        }
+        this.newChatConsumers.add(listener);
+    }
+
+}
