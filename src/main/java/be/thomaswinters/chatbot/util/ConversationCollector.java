2c7dfda024e1dafc0620250b8e383d5c69abbeda
T. Winters <info@thomaswinters.be>
2018-04-28 23:20:18 +0200

    Updated ConversationCollector to have a word counter mapper at the end

--- src/main/java/be/thomaswinters/chatbot/util/ConversationCollector.java
+++ src/main/java/be/thomaswinters/chatbot/util/ConversationCollector.java
@@ -4,6 +4,7 @@ import be.thomaswinters.chatbot.data.IChatMessage;
 import be.thomaswinters.wordcounter.WordCounter;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * Class to help collect conversations of chat messages with weights to every message
@@ -16,13 +17,23 @@ public class ConversationCollector {
     private final BiFunction<IChatMessage, Integer, Integer> ownMessagesWeightMapper;
     private final BiFunction<IChatMessage, Integer, Integer> otherMessagesWeightMapper;
 
+    private final Function<WordCounter, WordCounter> wordCounterMapper;
+
     public ConversationCollector(String userName, int maxMessages,
                                  BiFunction<IChatMessage, Integer, Integer> ownMessagesWeightMapper,
-                                 BiFunction<IChatMessage, Integer, Integer> otherMessagesWeightMapper) {
+                                 BiFunction<IChatMessage, Integer, Integer> otherMessagesWeightMapper,
+                                 Function<WordCounter, WordCounter> wordCounterMapper) {
         this.userName = userName.toLowerCase();
         this.maxMessages = maxMessages;
         this.ownMessagesWeightMapper = ownMessagesWeightMapper;
         this.otherMessagesWeightMapper = otherMessagesWeightMapper;
+        this.wordCounterMapper = wordCounterMapper;
+    }
+
+    public ConversationCollector(String userName, int maxMessages,
+                                 BiFunction<IChatMessage, Integer, Integer> ownMessagesWeightMapper,
+                                 BiFunction<IChatMessage, Integer, Integer> otherMessagesWeightMapper) {
+        this(userName, maxMessages, ownMessagesWeightMapper, otherMessagesWeightMapper, Function.identity());
     }
 
     public ConversationCollector(String username, int maxMessages) {
@@ -45,6 +56,6 @@ public class ConversationCollector {
                 break;
             }
         }
-        return b.build();
+        return wordCounterMapper.apply(b.build());
     }
 }
