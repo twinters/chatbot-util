b8d0f7b67df2ea50b7aa67d45aa8beafa8ad2bfb
T. Winters <info@thomaswinters.be>
2018-05-24 16:41:31 +0200

    Added getMentionName

--- src/main/java/be/thomaswinters/chatbot/data/IChatUser.java
+++ src/main/java/be/thomaswinters/chatbot/data/IChatUser.java
@@ -6,6 +6,10 @@ public interface IChatUser {
     Optional<String> getFullName();
 
     String getScreenName();
+    
+    default String getMentionName() {
+        return getScreenName();
+    }
 
     long getId();
 }
