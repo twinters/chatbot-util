656f515bf7a680abaac48f611462c27a64fbdd53
T. Winters <info@thomaswinters.be>
2018-05-24 18:27:26 +0200

    Made chatbot extend reacting generator

--- src/main/java/be/thomaswinters/chatbot/IChatBot.java
+++ src/main/java/be/thomaswinters/chatbot/IChatBot.java
@@ -1,9 +1,16 @@
 package be.thomaswinters.chatbot;
 
 import be.thomaswinters.chatbot.data.IChatMessage;
+import be.thomaswinters.generator.generators.reacting.IReactingGenerator;
 
 import java.util.Optional;
 
-public interface IChatBot {
+@FunctionalInterface
+public interface IChatBot extends IReactingGenerator<String,IChatMessage> {
     Optional<String> generateReply(IChatMessage message);
+
+    @Override
+    default Optional<String> generateRelated(IChatMessage input) {
+        return generateReply(input);
+    }
 }
